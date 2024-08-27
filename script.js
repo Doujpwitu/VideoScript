729855ea75ecf8db484682c68423f5bc7058936d
Ma3ri4 <94155834+Doujpwitu@users.noreply.github.com>
2024-08-27 18:05:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const player = videojs('vid1', {
      autoplay: 'muted',
      controls: true,
-     playsinline: true
+     playsinline: true,
+     playbackRates: [0.5, 1, 1.5, 2],
+     fluid: true,
+     controlBar: {
+        skipButtons: {
+          forward: 5,
+          backward: 5
+        }
+    },
+    userActions: {
+        hotkeys: true
+    }
 });
