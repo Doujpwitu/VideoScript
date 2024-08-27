70ac83ac24350dd761804eea3fae15a157d52bad
Ma3ri4 <94155834+Doujpwitu@users.noreply.github.com>
2024-08-27 18:21:28 +0300

    highlite when spoken working with video.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const player = videojs('vid1', {
      autoplay: 'muted',
      controls: true,
      playsinline: true,
-     playbackRates: [0.5, 1, 1.5, 2],
+     playbackRates: [0.5, 0.8 ,1],
      fluid: true,
      controlBar: {
         skipButtons: {
@@ -13,4 +13,32 @@ const player = videojs('vid1', {
     userActions: {
         hotkeys: true
     }
+});
+
+const scriptSpans = document.querySelectorAll('#script span');
+
+
+player.on('timeupdate', () => {
+    const currentTime = player.currentTime();
+
+    scriptSpans.forEach(span => {
+        const start = parseFloat(span.getAttribute('data-start'));
+        const end = parseFloat(span.getAttribute('data-end'));
+
+        if (currentTime >= start && currentTime <= end) {
+            span.classList.add('highlight');
+        } else {
+            span.classList.remove('highlight');
+        }
+    });
+});
+
+
+// Optional: Make script clickable to jump to specific time in the video
+scriptSpans.forEach(span => {
+    span.addEventListener('click', () => {
+        const start = parseFloat(span.getAttribute('data-start'));
+        player.currentTime(start);
+        player.play();
+    });
 });
